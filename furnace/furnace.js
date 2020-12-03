c68ca5025643d087ebfba25837203ac405c02525
ninjadev64 <awesomeBurger72@gmail.com>
2020-12-03 20:03:00 +0000

    z

--- furnace/furnace.js
+++
@@ -1,8 +0,0 @@
-function getSmeltingRecipe(s1, s2, printConsole) {
-    var result;
-    if (printConsole) {
-        console.log("Returning recipe for: " + s1 + " " + s2);
-    }
-    // code for returning recipe
-    return(result);
-}
